02bab75ce0192c09f4c5e2413a5e49cac0329361
Sergei Belokon <srj.belokon@gmail.com>
2025-04-03 21:02:29 +1100

    chore: add bootstrap script prototype

---
+++ dotfiles/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [[ $(uname -s) == 'Darwin' ]]; then
+	ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs ~/icloud
+fi
+
+mkdir -p ~/rps/github.com
+git clone git@github.com:enkron/env.git ~/rps/github.com/env
+
+ln -s ~/rps/github.com/env/dotfiles/Linux/gitconfig ~/.gitconfig
+ln -s ~/rps/github.com/env/dotfiles/Linux/vimrc ~/.vimrc
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
