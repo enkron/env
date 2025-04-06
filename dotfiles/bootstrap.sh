cb5fbbf36f88a48168224e5072474e9b3991c6b4
Sergei Belokon <srj.belokon@gmail.com>
2025-04-06 20:55:49 +1000

    chore(bootstrap): add determinate nix installation

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+
+install_nix() {
+    curl --proto =https --tlsv1.2 -sSf -L https://install.determinate.systems/nix |\
+        sh -s -- install --no-confirm
+}
+
 if [ $(uname -s) = 'Darwin' ]; then
 	ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs ~/icloud
 fi
@@ -11,3 +17,5 @@ ln -s ~/rps/github.com/env/dotfiles/Linux/gitconfig ~/.gitconfig
 ln -s ~/rps/github.com/env/dotfiles/Linux/vimrc ~/.vimrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+install_nix
