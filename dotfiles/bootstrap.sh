bfbd660ae72a4bc0d0a828bf9c441da42649232e
Sergei Belokon <srj.belokon@gmail.com>
2025-04-15 21:07:20 +1000

    refactor(bootstrap): refactor the bootstrap script according to POSIX standard

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 install_nix() {
-    curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix |\
+    curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
         sh -s -- install --determinate --no-confirm
 }
 
-if [ $(uname -s) = 'Darwin' ]; then
+if [ "$(uname -s)" = 'Darwin' ]; then
     # Add link to the Icloud storage on the filesystem
-	ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs ~/icloud
+    ln -s "${HOME}/Library/Mobile Documents/com~apple~CloudDocs" "${HOME}/icloud"
 
     # Disable MacOS press and hold feature in the Cursor app to allow
     # continuous key movement in Vim mode
-    if [ -x $(which cursor) ]; then
-        defaults write $(osascript -e 'id of app "Cursor"') ApplePressAndHoldEnabled -bool false
+    if command -v cursor >/dev/null 2>&1; then
+        defaults write "$(osascript -e 'id of app "Cursor"')" ApplePressAndHoldEnabled -bool false
     fi
 fi
 
-mkdir -p ~/rps/github.com
-git clone git@github.com:enkron/env.git ~/rps/github.com/env
+mkdir -p "${HOME}/rps/github.com"
+git clone git@github.com:enkron/env.git "${HOME}/rps/github.com/env"
 
-ln -s ~/rps/github.com/env/dotfiles/Linux/gitconfig ~/.gitconfig
-ln -s ~/rps/github.com/env/dotfiles/Linux/vimrc ~/.vimrc
+ln -s "${HOME}/rps/github.com/env/dotfiles/Linux/gitconfig" "${HOME}/.gitconfig"
+ln -s "${HOME}/rps/github.com/env/dotfiles/Linux/vimrc" "${HOME}/.vimrc"
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
 
 install_nix
