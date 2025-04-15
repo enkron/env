b3ea69a10306b1251854ea000d661311f7198747
Sergei Belokon <srj.belokon@gmail.com>
2025-04-15 15:09:11 +1000

    chore(nix): add kubectl v1.30.1 installation into the flake

--- dotfiles/Linux/nix_flakes/minimal_dev_example/flake.nix
+++ dotfiles/Linux/nix_flakes/minimal_dev_example/flake.nix
@@ -3,20 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-
     # Pinned to the commit where poetry v1.7.1 is available
     nixpkgs-poetry171.url = "github:NixOS/nixpkgs/087f43a1fa052b17efd441001c4581813c34bc19";
+    # Pinned to the commit where kubectl v1.30.1 is available
+    nixpkgs-kubectl130.url = "github:NixOS/nixpkgs/6eb01a67e1fc558644daed33eaeb937145e17696";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-poetry171 }:
+  outputs = { self, nixpkgs, nixpkgs-poetry171, nixpkgs-kubectl130 }:
     let
       system = "x86_64-linux";
 
       # Standard packages repository for the python 3.10 and other tools
       pkgs = import nixpkgs { inherit system; };
-
       # Pinned packages repository to the poetry v1.7.1
       pkgs-poetry171 = import nixpkgs-poetry171 { inherit system; };
+      # Pinned packages repository to the kubectl v1.30.1
+      pkgs-kubectl130 = import nixpkgs-kubectl130 { inherit system; };
     in {
       devShells.${system} = {
         python310 = pkgs.mkShell {
@@ -37,12 +39,15 @@
           name = "Basic toolset";
           paths = with pkgs; [
             bat
+          ] ++ [
+            pkgs-kubectl130.kubectl
           ];
         };
 
         exp = pkgs.buildEnv {
           name = "Experimental toolset";
           paths = with pkgs; [
+            # Other experimental tools
           ];
         };
       };
