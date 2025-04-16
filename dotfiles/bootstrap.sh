a7618688bcae39ad7a7025bb7c74700305fe5551
Sergei Belokon <srj.belokon@gmail.com>
2025-04-16 19:06:55 +1000

    chore(bootstrap): make the existing steps idempotent to the installed tools

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -1,27 +1,44 @@
 #!/bin/sh
 
+REPOS_HOME="${HOME}/rps/github.com"
+
 install_nix() {
     curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
         sh -s -- install --determinate --no-confirm
 }
 
+if [ -d "${REPOS_HOME}/env" ]; then
+    echo "env repository already exists, skipping clone step"
+else
+    git clone git@github.com:enkron/env.git "${REPOS_HOME}/env"
+fi
+
 if [ "$(uname -s)" = 'Darwin' ]; then
     # Add link to the Icloud storage on the filesystem
-    ln -s "${HOME}/Library/Mobile Documents/com~apple~CloudDocs" "${HOME}/icloud"
+    ln -sf "${HOME}/Library/Mobile Documents/com~apple~CloudDocs" "${HOME}/icloud"
 
     # Disable MacOS press and hold feature in the Cursor app to allow
     # continuous key movement in Vim mode
     if command -v cursor >/dev/null 2>&1; then
+        cursor_config_path="${HOME}/Library/Application Support/Cursor/User"
+
         defaults write "$(osascript -e 'id of app "Cursor"')" ApplePressAndHoldEnabled -bool false
+        ln -sf "${REPOS_HOME}/env/dotfiles/cursor/keybindings.json" "${cursor_config_path}/keybindings.json"
+        ln -sf "${REPOS_HOME}/env/dotfiles/cursor/settings.json" "${cursor_config_path}/settings.json"
     fi
 fi
 
-mkdir -p "${HOME}/rps/github.com"
-git clone git@github.com:enkron/env.git "${HOME}/rps/github.com/env"
+ln -sf "${REPOS_HOME}/env/dotfiles/Linux/gitconfig" "${HOME}/.gitconfig"
+ln -sf "${REPOS_HOME}/env/dotfiles/Linux/vimrc" "${HOME}/.vimrc"
 
-ln -s "${HOME}/rps/github.com/env/dotfiles/Linux/gitconfig" "${HOME}/.gitconfig"
-ln -s "${HOME}/rps/github.com/env/dotfiles/Linux/vimrc" "${HOME}/.vimrc"
+if [ -d "${HOME}/.vim/bundle/Vundle.vim" ]; then
+    echo "Vundle.vim already exists, skipping clone step"
+else
+    git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
+fi
 
-git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
-
-install_nix
+if command -v nix >/dev/null 2>&1; then
+    echo "nix already installed, skipping installation step"
+else
+    install_nix
+fi
