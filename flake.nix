e487c0f19efd14b3266a34947d403547d06424d2
Sergei Belokon <srj.belokon@gmail.com>
2025-04-22 16:43:39 +1000

    chore(nix): add kubectl v1.30.1 for Darwin OS
    dynamically choose between kubectl 1.30.1 commit hashes depending on
    platform

--- flake.nix
+++ flake.nix
@@ -6,20 +6,21 @@
     # Pinned to the commit where poetry v1.7.1 is available
     nixpkgs-poetry171.url = "github:NixOS/nixpkgs/087f43a1fa052b17efd441001c4581813c34bc19";
     # Pinned to the commit where kubectl v1.30.1 is available
-    nixpkgs-kubectl130.url = "github:NixOS/nixpkgs/6eb01a67e1fc558644daed33eaeb937145e17696";
+    nixpkgs-kubectl130-linux.url = "github:NixOS/nixpkgs/6eb01a67e1fc558644daed33eaeb937145e17696";
+    nixpkgs-kubectl130-darwin.url = "github:NixOS/nixpkgs/cca4f8e59e9479ced4f02f33530be367220d5826";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-poetry171, nixpkgs-kubectl130 }:
+  outputs = { self, nixpkgs, nixpkgs-poetry171, nixpkgs-kubectl130-linux, nixpkgs-kubectl130-darwin }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs (systems) f;
 
 
-      pkgs = forAllSystems(system: import nixpkgs { inherit system; });
-      # Pinned packages repository to the poetry v1.7.1
-      pkgs-poetry171 = forAllSystems(system: import nixpkgs-poetry171 { inherit system; });
-      # Pinned packages repository to the kubectl v1.30.1
-      pkgs-kubectl130 = forAllSystems(system: import nixpkgs-kubectl130 { inherit system; });
+      # pkgs = forAllSystems(system: import nixpkgs { inherit system; });
+      # # Pinned packages repository to the poetry v1.7.1
+      # pkgs-poetry171 = forAllSystems(system: import nixpkgs-poetry171 { inherit system; });
+      # # Pinned packages repository to the kubectl v1.30.1
+      # pkgs-kubectl130 = forAllSystems(system: import nixpkgs-kubectl130-linux { inherit system; });
 
       devShell = system:
         let
@@ -39,11 +40,15 @@
           };
         };
 
-        sysPkgs = system:
+      sysPkgs = system:
         let
           # Allow packages with non-free licencing scheme
           pkgs = import nixpkgs { system = system; config.allowUnfree = true;  };
-          kubectlPkgs = nixpkgs-kubectl130.legacyPackages.${system};
+          kubectlPkgs =
+            if pkgs.stdenv.isDarwin then
+              nixpkgs-kubectl130-darwin.legacyPackages.${system}
+            else
+              nixpkgs-kubectl130-linux.legacyPackages.${system};
         in {
           base = pkgs.buildEnv {
             name = "Basic toolset";
@@ -71,7 +76,7 @@
               kubectlPkgs.kubectl
             ];
           };
-      };
+        };
     in {
       devShells = forAllSystems devShell;
       packages = forAllSystems sysPkgs;
