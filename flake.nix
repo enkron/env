369d965941a8e7fdf41920dc0512762ddffff6f6
Sergei Belokon <srj.belokon@gmail.com>
2025-07-26 11:09:28 +1000

    chore(nix): upgrade kubectl version to 1.32

--- flake.nix
+++ flake.nix
@@ -8,15 +8,14 @@
     nixpkgs-poetry171.url = "github:NixOS/nixpkgs/087f43a1fa052b17efd441001c4581813c34bc19";
 
     # Pinned to the commit where kubectl v1.30.1 is available
-    nixpkgs-kubectl130-linux.url = "github:NixOS/nixpkgs/6eb01a67e1fc558644daed33eaeb937145e17696";
-    nixpkgs-kubectl130-darwin.url = "github:NixOS/nixpkgs/cca4f8e59e9479ced4f02f33530be367220d5826";
+    nixpkgs-kubectl132.url = "github:NixOS/nixpkgs/f8918a1e3ea5e5549b2c78996842c6c3b47da2a8";
 
     # In order for python310 package to work correctly `sphinx` dependency must be < 8.2.3 version
     # (this version doesn't support python3.10). Therefore pinning nixpkgs url to the previous hash
     nixpkgs-sphinx747.url = "github:NixOS/nixpkgs/a684c58d46ebbede49f280b653b9e56100aa3877";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-poetry171, nixpkgs-kubectl130-linux, nixpkgs-kubectl130-darwin, nixpkgs-sphinx747 }:
+  outputs = { self, nixpkgs, nixpkgs-poetry171, nixpkgs-kubectl132, nixpkgs-sphinx747 }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs (systems) f;
@@ -26,7 +25,7 @@
       # # Pinned packages repository to the poetry v1.7.1
       # pkgs-poetry171 = forAllSystems(system: import nixpkgs-poetry171 { inherit system; });
       # # Pinned packages repository to the kubectl v1.30.1
-      # pkgs-kubectl130 = forAllSystems(system: import nixpkgs-kubectl130-linux { inherit system; });
+      # pkgs-kubectl130 = forAllSystems(system: import nixpkgs-kubectl132 { inherit system; });
 
       devShell = system:
         let
@@ -64,9 +63,9 @@
           pkgs = import nixpkgs { system = system; config.allowUnfree = true;  };
           kubectlPkgs =
             if pkgs.stdenv.isDarwin then
-              nixpkgs-kubectl130-darwin.legacyPackages.${system}
+              nixpkgs-kubectl132.legacyPackages.${system}
             else
-              nixpkgs-kubectl130-linux.legacyPackages.${system};
+              nixpkgs-kubectl132.legacyPackages.${system};
         in {
           base = pkgs.buildEnv {
             name = "Basic toolset";
